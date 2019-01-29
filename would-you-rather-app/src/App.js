3f315288acd4fbdc59dc35d87202810c951088c5
Matt Coleman <coleman.matt319@gmail.com>
2019-01-29 10:55:00 -0500

    Added templates for question list to home page.

--- would-you-rather-app/src/App.js
+++ would-you-rather-app/src/App.js
@@ -2,27 +2,67 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const imgUrl = "https://kooledge.com/assets/default_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png"
     return (
       <div>
         <div>
-          <ul class="nav">
+          <ul className="nav">
             <li id="brand">WYR?</li>
             <li><a href="#">Home</a></li>
             <li><a href="#">New Question</a></li>
             <li><a href="#">Leader Board</a></li>
             <li id="logout"><a href="#">Logout</a></li>
+            <li id="hello">Hello, user!</li>
           </ul>
         </div>
-        <div class="container">
-          <div class="filter-btn">
+        <div className="container">
+          <div className="filter-btn">
             <button>Unanswered Questions</button>
             <button>Answered Questions</button>
           </div>
-          <div class="panel">
-            <ul class="questions">
-              <li>Question 1</li>
-              <li>Question 2</li>
-              <li>Question 3</li>
+          <div className="panel">
+            <ul className="question-list">
+              <li>
+                <div className="question">
+                  <span id="panel-left"><img src={imgUrl} alt="Your face here"/></span>
+                  <span id="panel-right"><p>Would you rather...</p></span>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
+              <li>
+                <div className="question">
+                  <img src={imgUrl} alt="Your face here"/>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
