622f37651f31f41e7cd304b21f9a844495d2701c
Matt Coleman <coleman.matt319@gmail.com>
2019-02-07 21:11:01 -0500

    Started onChange handlers for radio inputs.

--- would-you-rather-app/src/components/PollView.js
+++ would-you-rather-app/src/components/PollView.js
@@ -3,19 +3,35 @@ import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 class PollView extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+
+    this.image = {
+      URL: "https://kooledge.com/assets/default_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png"
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
   handleSubmit = (e) => {
     e.preventDefault()
   }
 
+  handleInputChange = (e) => {
+    e.preventDefault()
+  }
+
   render() {
-    const imgUrl = "https://kooledge.com/assets/default_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png"
-    console.log(this.props)
+
     const { user, poll } = this.props
     return (
       <div className="poll-container">
         <div className="question-info">
-          <img src={imgUrl} alt="Your face here"/>
+          <img src={this.image.URL} alt="Your face here"/>
           <h4>{user}</h4>
         </div>
         <div className="question-info">
@@ -24,12 +40,14 @@ class PollView extends Component {
             <label>
               <input type="radio"
                     name="poll-option"
-                    value="Something" required/> {poll.optionOne.text}
+                    value="optionOne"
+                    onChange={this.handleInputChange}/> {poll.optionOne.text}
             </label><br/>
             <label>
               <input type="radio"
                     name="poll-option"
-                    value="Something else"/> {poll.optionTwo.text}
+                    value="optionTwo"
+                    onChange={this.handleInputChange}/> {poll.optionTwo.text}
             </label>
             <button id="poll-button" onClick={this.handleSubmit}>
               <NavLink to={`/results/${poll.id}`}>Submit</NavLink>
