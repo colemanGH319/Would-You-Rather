254465e7d9182fa5b85748cd09925c2c67f5021c
Matt Coleman <coleman.matt319@gmail.com>
2019-02-10 17:41:52 -0500

    Update: created handlers for radio buttons, submit input.

--- would-you-rather-app/src/components/PollForm.js
+++ would-you-rather-app/src/components/PollForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {handleQuestionAnswer} from '../actions/questions'
 
 class PollForm extends Component {
 
@@ -6,25 +7,39 @@ class PollForm extends Component {
     value: ''
   }
 
+  handleInputChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitting")
+    this.props.dispatch(handleQuestionAnswer)
+  }
+
   render() {
     const { poll } = this.props
     return (
       <div>
         <h2>Would you rather?</h2>
-        <form action="#">
+        <form action="#" onSubmit={this.handleSubmit}>
           <label>
             <input type="radio"
                   name="poll-option"
                   value="optionOne"
+                  checked={this.state.value === 'optionOne'}
                   onChange={this.handleInputChange}/> {poll.optionOne.text}
           </label><br/>
           <label>
             <input type="radio"
                   name="poll-option"
                   value="optionTwo"
+                  checked={this.state.value === 'optionTwo'}
                   onChange={this.handleInputChange}/> {poll.optionTwo.text}
           </label>
-          <input type="submit" id="poll-button" value="Submit" onClick={this.handleSubmit}/>
+          <input type="submit" id="poll-button" value="Submit" />
         </form>
       </div>
     )
