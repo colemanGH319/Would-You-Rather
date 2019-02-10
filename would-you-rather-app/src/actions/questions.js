5b244ed1cb712d361c229861c5e7bb59b98ea6b7
Matt Coleman <coleman.matt319@gmail.com>
2019-02-09 21:00:40 -0500

    Update: added action creator for new questions.

--- would-you-rather-app/src/actions/questions.js
+++ would-you-rather-app/src/actions/questions.js
@@ -1,8 +1,16 @@
 export const SET_QUESTIONS = 'SET_QUESTIONS'
+export const CREATE_QUESTION = 'CREATE_QUESTION'
 
 export function setQuestions(questions){
   return{
     type: SET_QUESTIONS,
     questions: questions,
+  }
+}
+
+export function createQuestion(question){
+  return {
+    type: CREATE_QUESTION,
+    question: question
   }
 }
