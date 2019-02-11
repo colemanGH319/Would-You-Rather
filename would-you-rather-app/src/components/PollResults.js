e288ba91cffad380b5395c8a1b77bc3e73dabbf3
Matt Coleman <coleman.matt319@gmail.com>
2019-02-10 23:39:37 -0500

    Created PollResults component.

---
+++ would-you-rather-app/src/components/PollResults.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class PollResults extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Results</h2>
+      </div>
+    )
+  }
+}
+
+export default PollResults
