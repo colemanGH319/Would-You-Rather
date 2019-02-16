d7f1a9deacc472b58c3dbe467296f9fb00422466
Matt Coleman <coleman.matt319@gmail.com>
2019-02-15 23:57:11 -0500

    Calculate poll results.

--- would-you-rather-app/src/components/PollResults.js
+++ would-you-rather-app/src/components/PollResults.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
-
 class PollResults extends Component {
 
   render() {
+    const { poll } = this.props
+    const resultOne = poll.optionOne.votes.length
+    const resultTwo = poll.optionTwo.votes.length
     return (
       <div>
         <h2>Results</h2>
+        <p>{poll.optionOne.text + ": " + (resultOne / (resultOne + resultTwo)) * 100 + '%'}</p>
+        <p>{poll.optionTwo.text + ": " + (resultTwo / (resultOne + resultTwo)) * 100 + '%'}</p>
       </div>
     )
   }
