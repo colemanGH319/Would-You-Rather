796d48cfdd97d9845eca9e6f5fd46a84c604f12d
Matt Coleman <coleman.matt319@gmail.com>
2019-02-18 13:33:02 -0500

    Small fixes to code style

--- would-you-rather-app/src/actions/questions.js
+++ would-you-rather-app/src/actions/questions.js
@@ -1,7 +1,8 @@
-import { _saveQuestion } from '../_DATA.js'
+import { _saveQuestion, _saveQuestionAnswer } from '../_DATA.js'
 
 export const SET_QUESTIONS = 'SET_QUESTIONS'
 export const CREATE_QUESTION = 'CREATE_QUESTION'
+export const CAST_VOTE = 'CAST_VOTE'
 
 export function setQuestions(questions){
   return{
@@ -27,5 +28,24 @@ export function handleNewQuestion(question) {
         .catch((e) => {
           alert(e)
         })
+  }
+}
+
+export function castVote(question) {
+  return {
+    type: CAST_VOTE,
+    question: question
+  }
+}
+
+export function handleQuestionAnswer(question) {
+  return (dispatch) => {
+    return _saveQuestionAnswer(question)
+          .then(() => {
+            dispatch(castVote(question))
+          })
+          .catch((e) => {
+            alert(e)
+          })
   }
 }
