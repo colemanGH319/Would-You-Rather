01d5193464ad3f4818bfa000e114eee42e7684ff
Matt Coleman <coleman.matt319@gmail.com>
2019-02-18 21:40:20 -0500

    Updated image URL to tinyURL.

--- would-you-rather-app/src/components/Question.js
+++ would-you-rather-app/src/components/Question.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
 class Question extends Component {
+  constructor(props){
+    super(props)
+
+    this.image = {
+      URL:  "https://tinyurl.com/yxeb5esp"
+    }
+  }
 
   render() {
-    const imgUrl = "https://kooledge.com/assets/default_medium_avatar-57d58da4fc778fbd688dcbc4cbc47e14ac79839a9801187e42a796cbd6569847.png"
-
     return (
         <div className="question">
           <div className="question-info">
-            <img src={imgUrl} alt="Your face here"/>
+            <img src={this.image.URL} alt="Your face here"/>
             <h4>{this.props.user}</h4>
           </div>
           <div className="question-info">
