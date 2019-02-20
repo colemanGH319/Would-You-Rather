00119bc973e41bfb671c2ac793d15fee79a6a28d
Matt Coleman <coleman.matt319@gmail.com>
2019-02-20 10:52:22 -0500

    Imported Switch component, route users to ErrorPage whenever path doesn't find a match

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Nav from './components/Nav'
 import QuestionList from './components/QuestionList'
 import PollView from './components/PollView'
 import Login from './components/Login'
+import ErrorPage from './components/ErrorPage'
 import LeaderBoard from './components/LeaderBoard'
 import NewQuestion from './components/NewQuestion'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { handleInitialData } from './actions/shared'
 import { connect } from 'react-redux'
 
@@ -22,11 +23,15 @@ class App extends Component {
               ? (<div className="loading-page"><h2>Loading...</h2></div>)
               : (<div>
                   <Nav />
-                  <Route exact path='/' component={QuestionList} />
-                  <Route path='/questions/:id' component={PollView} />
-                  <Route path='/login' component={Login}/>
-                  <Route path='/add' component={NewQuestion} />
-                  <Route path='/leaderboard' component={LeaderBoard}/>
+                  <Switch>
+                    <Route exact path='/' component={QuestionList} />
+                    <Route path='/questions/:id' component={PollView} />
+                    <Route path='/login' component={Login}/>
+                    <Route path='/add' component={NewQuestion} />
+                    <Route path='/leaderboard' component={LeaderBoard}/>
+                    <Route component={ErrorPage} />
+                  </Switch>
+
                 </div>)}
         </Fragment>
       </Router>
