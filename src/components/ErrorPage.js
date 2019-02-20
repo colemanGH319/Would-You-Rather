42816b21d75c5c85397707366681444bddae81e2
Matt Coleman <coleman.matt319@gmail.com>
2019-02-20 10:53:39 -0500

    Created ErrorPage to render any time path doesn't match or question ID not found

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class ErrorPage extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <div className="headline">
+          <h2>404 - Page Not Found</h2>
+        </div>
+      </div>
+    )
+  }
+}
